aaf30cd6ef957bd986f408bbca8c436fa1cf7c1b
l1kholetov <likholetov.vova@bk.ru>
2024-08-07 13:49:13 +0300

    added the ability to search for employees by list, as well as its filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,9 @@ class App extends Component {
 				{name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
 				{name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
 				{name: 'Carl W.', salary: 5000, increase: false, rise: false, id: 3}
-			]
+			],
+			term: '',
+			filter: 'all'
 		};
 		this.maxId = 4;
 	}
@@ -57,9 +59,42 @@ class App extends Component {
 		}))
 	}
 
+	searchEmp = (items, term) => {
+		if (term.length === 0) {
+			return items;
+		}
+
+		return items.filter(item => {
+			return item.name.indexOf(term) > -1
+		})
+	}
+
+	onUpdateSearch = (term) => {
+		this.setState({term});
+	}
+
+	filterPost = (items, filter) => {
+		switch (filter) {
+			case 'rise':
+				return items.filter(item => item.rise);
+
+			case 'moreThen1000':
+					return items.filter(item => item.salary > 1000);
+
+			default:
+				return items;
+		}
+	}
+
+	onFilterSelect = (filter) => {
+		this.setState({filter});
+	}
+
 	render() {
-		const employees = this.state.data.length,
-					increased = this.state.data.filter(item => item.increase).length
+		const {data, term, filter} = this.state,
+					employees = this.state.data.length,
+					increased = this.state.data.filter(item => item.increase).length,
+					visibleData = this.filterPost(this.searchEmp(data, term), filter);
 
 		return (
 			<div className="app">
@@ -68,12 +103,12 @@ class App extends Component {
 					increased={increased}/>
 
 				<div className="search-panel">
-					<SearchPanel/>
-					<AppFilter/>
+					<SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+					<AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
 				</div>
 
 				<EmployeesList
-					data={this.state.data}
+					data={visibleData}
 					onDelete={this.deleteItem}
 					onToggleProp={this.onToggleProp}/>
 
